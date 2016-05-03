a39a803588d25b036c55f073787c7fdadc6ebb4c
GoZSY <13207508051@163.com>
2016-05-03 08:32:10 +0800

    Create The-Kth-node-binary-search-trees.cpp

---
+++ The-Kth-node-binary-search-trees.cpp
@@ -0,0 +1,54 @@
+/*
+struct TreeNode {
+    int val;
+    struct TreeNode *left;
+    struct TreeNode *right;
+    TreeNode(int x) :
+            val(x), left(NULL), right(NULL) {
+    }
+};
+*/
+class Solution {
+    int count = 0;
+public:
+    TreeNode* KthNode(TreeNode* pRoot, unsigned int k)
+    {
+        if(pRoot)
+        { 
+            TreeNode *ret = KthNode(pRoot->left, k);
+            if(ret) return ret;
+            if(++count == k) return pRoot;
+            ret = KthNode(pRoot->right,k);
+            if(ret) return ret;
+        }
+        return NULL;
+    }
+};
+
+/*********************************************************
+更好理解的代码
+**********************************************************/
+
+class Solution {
+public:
+    TreeNode* KthNode(TreeNode* pRoot, unsigned int k)
+    {
+  TreeNode *tr=NULL;
+  unsigned int count =0;
+  KthNode1(pRoot,&tr,count,k);
+  return tr;
+    }
+ void KthNode1(TreeNode* pRoot, TreeNode** tr,unsigned int &count,unsigned int k)
+ {
+  if(pRoot==NULL)
+   return ;
+  KthNode1(pRoot->left,tr,count,k);
+  count++;
+  if(k==count)
+  {
+   *tr=pRoot;
+   return;
+  }
+  KthNode1(pRoot->right,tr,count,k);
+ }
+};
